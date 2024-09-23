5105afb2975b167c9a7f6851740aca28ae3ffca4
mapaixinterieure <melissa.braun@outlook.de>
2024-09-23 21:25:13 +0200

    added axios to index.html

--- src/weather_app.js
+++ src/weather_app.js
@@ -1,1 +1,11 @@
-alert ("hello world");
+function handleSearchSubmit(event){
+    event.preventDefault();
+
+    let searchInput = document.querySelector("#search_form_input");
+
+    let weather_app_cityElement = document.querySelector("#weather_app_city")
+    weather_app_cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search_form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
